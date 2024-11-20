70557ec32066a84ff9ab9c856377b3d6f0a5be3a
simon <simon@klein-homepage.de>
2024-11-20 06:26:32 +0200

    v0.1.19

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.18'
+VERSION = '0.1.19'
 DESCRIPTION = 'IceCreamSwap Web3.py wrapper'
 LONG_DESCRIPTION = 'IceCreamSwap Web3.py wrapper with automatic retries, multicall and other advanced functionality'
 
