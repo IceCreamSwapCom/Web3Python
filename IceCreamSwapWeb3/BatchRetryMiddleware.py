22bd5ec475c7cf9a4471c83e418ca67a7b1ab610
simon <simon@klein-homepage.de>
2024-12-01 16:22:31 +0200

    batch retry and splitting now is more sophisticated, where individual failed calls are detected and retried.

--- IceCreamSwapWeb3/BatchRetryMiddleware.py
+++ IceCreamSwapWeb3/BatchRetryMiddleware.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from web3.middleware import Web3Middleware
 
 from IceCreamSwapWeb3 import Web3Advanced
@@ -16,12 +18,33 @@ class BatchRetryMiddleware(Web3Middleware):
 
             try:
                 response = make_batch_request(requests_info)
-                if len(response) == len(requests_info):
-                    return response
-            except Exception:
+            except Exception as e:
                 if len(requests_info) == 1:
-                    raise
+                    print(f"batch RPC call with single request got exception {repr(e)}, waiting 1 sec and trying again")
+                    sleep(1)
+                    return middleware(requests_info)
+                print(f"batch RPC call with {len(requests_info)} requests got exception {repr(e)}, splitting and retrying")
+            else:
+                if len(response) == len(requests_info):
+                    # find individual failed requests
+                    requests_retry = []
+                    request_indexes: list[tuple[int, int]] = []
+                    for i, (request_single, response_single) in enumerate(zip(requests_info, response)):
+                        if "error" in response_single or response_single["result"] is None:
+                            request_indexes.append((i, len(requests_retry)))
+                            requests_retry.append(request_single)
 
+                    if len(requests_retry) != 0:
+                        # retry failed requests
+                        print(f"{len(requests_retry)}/{len(requests_info)} requests in batch failed, retrying. Example response: {response[request_indexes[0][0]]}")
+                        if len(requests_retry) == len(requests_info):
+                            # all failed, let's wait a moment before retrying
+                            sleep(1)
+                        response_new = middleware(requests_retry)
+                        for old_idx, new_idx in request_indexes:
+                            response[old_idx] = response_new[new_idx]
+
+                    return response
             middle = len(requests_info) // 2
             return middleware(requests_info[:middle]) + middleware(requests_info[middle:])
         return middleware
