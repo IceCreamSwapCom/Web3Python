f50cb35c1eeab3c7ae574f44a4ae0952064ddea5
simon <simon@klein-homepage.de>
2025-06-25 15:52:49 +0300

    v0.1.61 preventing invalid parsing of some RPC errors as ContractLogicError's

--- IceCreamSwapWeb3/Web3ErrorHandlerPatch.py
+++ IceCreamSwapWeb3/Web3ErrorHandlerPatch.py
@@ -103,14 +103,9 @@ def raise_contract_logic_error_on_revert(response: RPCResponse) -> RPCResponse:
 
     message = error.get("message")
     message_present = message is not None and message != ""
-    data = error.get("data")
+    data = error.get("data", MISSING_DATA)
 
-    if data is None:
-        if message_present:
-            raise ContractLogicError(message, data=MISSING_DATA)
-        elif not message_present:
-            raise ContractLogicError("execution reverted", data=MISSING_DATA)
-    elif isinstance(data, dict) and message_present:
+    if isinstance(data, dict) and message_present:
         raise ContractLogicError(f"execution reverted: {message}", data=data)
     elif isinstance(data, str):
         _raise_contract_error(data)
