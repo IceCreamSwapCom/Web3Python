fe432f0fd48e4550eb6e22cbb64748be4e61801b
simon <simon@klein-homepage.de>
2025-11-26 22:09:34 +0200

    v0.3.6 updated Base, BSC and Arbitrum Multicall to now accept native transfers

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.5'
+VERSION = '0.3.6'
 DESCRIPTION = 'IceCreamSwap Web3.py wrapper'
 LONG_DESCRIPTION = 'IceCreamSwap Web3.py wrapper with automatic retries, multicall and other advanced functionality'
 
